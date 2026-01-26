43e9709c28f184e6644dddc057c61e657da64957
Palpitaco JB <contato@palpitacojb.com.br>
2026-01-26 18:57:37 -0300

    fix(centenas): refatoração final + utils unificados + correções de horário

---
+++ src/pages/Centenas/CentenasPage.jsx
@@ -0,0 +1,11 @@
+﻿import React from "react";
+import Centenas from "./Centenas.jsx";
+
+/**
+ * Wrapper de compatibilidade:
+ * - Mantém o Centenas.jsx atual funcionando
+ * - Permite refatorar aos poucos para CentenasView.jsx sem quebrar rotas/imports
+ */
+export default function CentenasPage() {
+  return <Centenas />;
+}
