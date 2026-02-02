cba9c52f09245f92d8f4cf34158029f46f159703
Palpitaco JB <contato@palpitacojb.com.br>
2026-02-01 22:39:15 -0300

    chore: prepare backend for production deploy

---
+++ backend/scripts/runAutoImport.ps1
@@ -0,0 +1,17 @@
+param(
+  [string]$Lottery="PT_RIO",
+  [string]$ApiBase="http://127.0.0.1:3333",
+  [string]$Date=""
+)
+
+cd C:\Users\glaub\palpitaco
+
+$env:GOOGLE_APPLICATION_CREDENTIALS="C:\Users\glaub\palpitaco\backend\secrets\serviceAccount.json"
+$env:LOTTERY=$Lottery
+$env:PITACO_API_BASE=$ApiBase
+$env:VERIFY_PERSISTED="1"
+$env:FAIL_ON_CRITICAL="0"
+
+if ($Date) { $env:DATE=$Date } else { Remove-Item Env:\DATE -ErrorAction SilentlyContinue }
+
+node .\backend\scripts\autoImportToday.js
