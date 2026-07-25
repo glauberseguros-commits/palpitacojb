b0cf5262a04363b67662ddda3dc5d77deaa293a3
Glauber Oliveira <glauber.seguros@gmail.com>
2026-07-25 13:09:54 -0300

    refactor(shared): exportar contrato V3 do predictiveMilharEngine

--- src/shared/predictiveMilharEngine/index.js
+++ src/shared/predictiveMilharEngine/index.js
@@ -9,7 +9,7 @@
  * - não altera resultados;
  * - não altera o motor preditivo de grupos do Top3.
  *
- * O módulo preserva temporariamente o comportamento do motor V2
+ * O módulo preserva temporariamente os comportamentos dos motores V2 e V3
  * enquanto os consumidores são migrados de forma independente.
  */
 
@@ -24,6 +24,11 @@ export {
   diversifyMilharRecommendations,
   buildMilharAudit,
 } from "../../pages/Centenas/modules/milharProbabilityEngine";
+
+export {
+  buildMilharRecommendationV3,
+  diversifyMilharRecommendationsV3,
+} from "../../pages/Centenas/modules/milharProbabilityEngineV3";
 
 export const PREDICTIVE_MILHAR_ENGINE_CONTRACT =
   "PREDICTIVE_MILHAR_ENGINE_SHARED_V1";
