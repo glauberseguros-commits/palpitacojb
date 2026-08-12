4a496d8e898c49ae563a6785f9e8eb8557a977cb
Glauber Oliveira <glauber.seguros@gmail.com>
2026-08-12 11:47:20 -0300

    chore: remove legacy audit and tmp artifacts

--- tmp/lkn83b_exports.cjs
+++
@@ -1,4 +0,0 @@
-const mod = require("../backend/scripts/importKingApostas.js");
-
-console.log("EXPORTS:");
-console.log(Object.keys(mod).sort());
--- tmp/top3_ref_backup_20260724_183657/Top3.jsx
+++
@@ -1,9 +0,0 @@
-// src/pages/Top3/Top3.jsx
-import React from "react";
-import { useTop3Controller } from "./top3.hooks";
-import Top3View from "./Top3View";
-
-export default function Top3() {
-  const c = useTop3Controller();
-  return <Top3View {...c} />;
-}
